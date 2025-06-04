6e1de4f68369c9ff6004656c53d4033ee4e26f0d
AlbaDuqueC <alba.duque@iesnervion.es>
2025-06-04 21:26:05 +0200

    a

--- src/string/criptograma/Criptograma.java
+++ src/string/criptograma/Criptograma.java
@@ -12,36 +12,123 @@ public class Criptograma {
 			"aprende a vivir y sabras morir bien", "cada dia sabemos mas y entendemos menos",
 			"el dinero no puede comprar la vida", "la verdadera sabiduria esta en reconocer la propia ignorancia" };
 
-	static String fraseReal="";
-	
-	static String fraseCodificada="";
-	
+	static String fraseReal = "";
+
+	static String fraseCodificada = "";
+
 	public static void desordenaAbecedario() {
-		
-		Random rand= new Random();
-		
+
+		Random rand = new Random();
+
 		int iRand;
 		int jRand;
-		
-		char[][] nuevoAbecedario= new char[abecedario.length][abecedario.length];
-		
-		for(int i=0; i<abecedario.length; i++) {
-			
-			for(int j=0; j<abecedario.length; j++) {
-				
+
+		char[][] nuevoAbecedario = new char[abecedario.length][abecedario.length];
+
+		for (int i = 0; i < abecedario.length; i++) {
+
+			for (int j = 0; j < abecedario.length; j++) {
+
 				do {
-				iRand=rand.nextInt(0, nuevoAbecedario.length);
-				jRand=rand.nextInt(0, nuevoAbecedario[0].length);
-				
-				}while(nuevoAbecedario[iRand][jRand]);
-				
+					iRand = rand.nextInt(0, nuevoAbecedario.length);
+					jRand = rand.nextInt(0, nuevoAbecedario[0].length);
+
+				} while (String.valueOf(nuevoAbecedario[iRand][jRand]) != null);
+
+				nuevoAbecedario[iRand][jRand] = abecedario[i][j];
+
 			}
-			
+
 		}
-		
-		
-		
-		
+
+		abecedario = nuevoAbecedario;
+
 	}
-	
+
+	public static void eligeFrase() {
+
+		Random rand = new Random();
+
+		int num = rand.nextInt(0, frases.length);
+
+		fraseReal = frases[num];
+
+	}
+
+	public static void codificaFrase() {
+
+		boolean res = false;
+
+		int i = 0;
+		int j = 0;
+
+		for (int k = 0; k < fraseReal.length(); k++) {
+
+			while (i < abecedario.length && !res) {
+
+				while (j < abecedario[0].length && !res) {
+
+					if (abecedario[i][j] == fraseReal.charAt(k)) {
+
+						fraseCodificada += i + "" + j + " ";
+
+						res = true;
+
+					}
+
+				}
+
+			}
+
+			res = false;
+
+		}
+
+	}
+
+	public static boolean compruebaCodigo(int codigo, char letra) {
+
+		boolean res = false;
+
+		int iNum = codigo / 10;
+		int jNum = codigo % 10;
+
+		if (abecedario[iNum][jNum] == letra) {
+
+			fraseCodificada.replace(String.valueOf(codigo), String.valueOf(letra));
+
+			res = true;
+
+		}
+
+		return res;
+
+	}
+
+	public static boolean sonIguales() {
+
+		boolean res;
+
+		String fraseCod = "";
+
+		for (int i = 0; i < fraseCodificada.length(); i++) {
+
+			if (fraseCodificada.charAt(i) == ' ' && fraseCodificada.charAt(i + 1) == ' ') {
+
+				fraseCod += " ";
+
+			} else if (fraseCodificada.charAt(i) != ' ') {
+
+				fraseCod += String.valueOf(fraseCodificada.charAt(i));
+
+			}
+
+		}
+
+		res = fraseCod.equals(fraseReal);
+
+		return res;
+
+	}
+
 }
--- src/string/criptograma/PrincipalCriptograma.java
+++ src/string/criptograma/PrincipalCriptograma.java
@@ -3,7 +3,7 @@ package string.criptograma;
 public class PrincipalCriptograma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
